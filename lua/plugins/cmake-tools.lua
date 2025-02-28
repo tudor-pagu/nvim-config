94415a42d336c5e1b5161d97ec6f339f8eb45956
tudor <pagu.tudor@gmail.com>
2025-02-28 17:49:34 +0100

    add keymaps for cmake-tools

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,1 +1,21 @@
-return { "Civitasv/cmake-tools.nvim", lazy = false }
+return {
+    "Civitasv/cmake-tools.nvim",
+    lazy = false,
+    init = function()
+        vim.keymap.set(
+            "n",
+            "<Leader>mb",
+            "<cmd>CMakeBuild<cr>",
+            { noremap = true, silent = true, desc = "CMake Build" }
+        )
+
+        vim.keymap.set("n", "<Leader>mr", "<cmd>CMakeRun<cr>", { noremap = true, silent = true, desc = "CMake Run" })
+
+        vim.keymap.set(
+            "n",
+            "<Leader>mg",
+            "<cmd>CMakeGenerate<cr>",
+            { noremap = true, silent = true, desc = "CMake Generate" }
+        )
+    end,
+}
